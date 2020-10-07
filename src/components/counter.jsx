1b9c58458d77c9c73bb618ee56b1f2d70954ffb5
Nirmal Kumar <er.kumarnirmal@gmail.com>
2020-10-07 12:29:16 +0530

    New Changes

---
+++ src/components/counter.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class Counter extends Component {
+  render() { 
+    return <h4>Hello World,This is the changed logic</h4>
+  }
+
+}
+ 
+export default Counter;
